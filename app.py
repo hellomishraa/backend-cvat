ae12ab00f1e3d974873cc9be89d29cbe425d3737
Shantanu <shanmishra884@gmail.com>
2025-06-04 16:41:49 +0530

    Cors, all origin allowed

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import base64
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"": {"origins": "", "supports_credentials": True}})
+# CORS(app, resources={r"": {"origins": "", "supports_credentials": True}})
+CORS(app, origins="*")  # Allow all origins
 
 @app.route('/get-categories', methods=['POST'])
 def get_categories():
